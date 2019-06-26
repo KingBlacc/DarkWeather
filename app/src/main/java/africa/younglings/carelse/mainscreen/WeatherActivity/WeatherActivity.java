4266bd4051f897f07088c40922dc62da8bbb7575
Bobby Carelse <42929130+KingBlacc@users.noreply.github.com>
2019-06-26 18:28:33 +0200

    Working request

--- app/src/main/java/africa/younglings/carelse/mainscreen/WeatherActivity/WeatherActivity.java
+++ app/src/main/java/africa/younglings/carelse/mainscreen/WeatherActivity/WeatherActivity.java
@@ -1,8 +1,13 @@
 package africa.younglings.carelse.mainscreen.WeatherActivity;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -10,6 +15,24 @@ import africa.younglings.carelse.mainscreen.R;
 
 public class WeatherActivity extends AppCompatActivity implements IWeatherView{
 
+    //Layouts
+    private ConstraintLayout layoutWeather;
+    private LinearLayout layoutRain;
+
+    //Widgets
+    private TextView tvLocation;
+    private TextView tvTime;
+    private TextView tvTemperature;
+    private ImageView weatherIcon;
+    private TextView tvConditions;
+    private TextView tvWind;
+    private TextView tvWindConditions;
+    private TextView tvPrecipitation;
+    private TextView tvPrecipitationConditions;
+    private TextView tvHumidity;
+    private TextView tvHumidityConditions;
+    private RecyclerView recyclerView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
